553d74e410a412cf3aaaec86546a65905f81ff52
Peter Eck <jpeckiii@gmail.com>
2019-11-01 15:28:39 -0500

    Fix root route

---
+++ build/precache-manifest.f14b54f7ea7331a153a46645d987eb03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29337635d3b62f76475caa935371ff56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291f1fc9eceddfd87c82",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "98ace8fedb28478a6e89",
+    "url": "/static/js/2.ed6d0a8d.chunk.js"
+  },
+  {
+    "revision": "291f1fc9eceddfd87c82",
+    "url": "/static/js/main.a0fa79bf.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
