e4eb06e840938f597da1d0c69fb90f988a45edf8
Peter Eck <jpeckiii@gmail.com>
2019-11-03 11:29:51 -0600

    Add person model

---
+++ models/person.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const url = process.env.MONGO_URI;
+
+mongoose
+    .connect(url, { useNewUrlParser: true })
+    .then(result => console.log(`CONNECTED TO MONGO DB`))
+    .catch(e => console.warn(`ERROR CONNECTING TO MONGO DB`))
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+module.exports = mongoose.model('Person', personSchema);
