68ef3769630795bdeeef29b0248ae256eeaab9f0
Peter Eck <jpeckiii@gmail.com>
2019-11-03 13:03:29 -0600

    Set useFindAndModify false for Mongoose

--- models/person.js
+++ models/person.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+mongoose.set('useFindAndModify', false);
+
 console.log(`Connecting to ${ process.env.MONGO_URI }`)
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
